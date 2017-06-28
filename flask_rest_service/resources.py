25e1955e3d7ebd41f3c10050a1c8d1aa8e6143e0
Chris Walker <clwalker@advent.com>
2017-06-28 04:25:27 -0400

    CLeaning up Todd's Mama and her resources.py

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -9,9 +9,8 @@ import requests
 class Root(restful.Resource):
     def get(self):
         webhook_url = 'https://hooks.slack.com/services/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'
-        headers = { 'content-type': 'application/json'}
+        headers = { 'content-type': 'application/json' }
         payload = json.dumps({
-            'channel': '#gameday',
             'text': 'I have integrated this proof of concept, from an API server on Heroku to the webhook that posted this message. This took way longer than it fucking should have.'
         })
         request = requests.post(webhook_url, headers=headers, data=payload)
