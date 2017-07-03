6bd8e47666dc8c5cbc24d7aacc257e9495dd2d1b
Chris Walker <clwalker@advent.com>
2017-07-03 18:02:34 -0400

    Added Todd's Mama's scoreboard, proof of concept

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -5,19 +5,42 @@ from flask.ext.restful import reqparse
 from flask_rest_service import app, api, mongo
 from bson.objectid import ObjectId
 import requests
+from espnff import League
+
+LEAGUE_ID = 367562
+WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+
+def post_to_slack(text):
+    headers = { 'content-type': 'application/json' }
+    payload = json.dumps({
+        'text': text
+    })
+    return requests.post(WEBHOOK_URL, headers=headers, data=payload)
 
 class Root(restful.Resource):
     def get(self):
-        webhook_url = 'https://hooks.slack.com/services/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'
-        headers = { 'content-type': 'application/json' }
-        payload = json.dumps({
-            'text': 'I have integrated this proof of concept, from an API server on Heroku to the webhook that posted this message. This took way longer than it fucking should have.'
-        })
-        request = requests.post(webhook_url, headers=headers, data=payload)
-
+        league = League(LEAGUE_ID, 2016),
         return {
             'status': 'OK',
             'mongo': str(mongo.db),
+            'league': league,
+            'scoreboard': league.scoreboard(week=1)
         }
 
+class Scoreboard(restful.Resource):
+    def post(self):
+        args = self.parser.parse_args()
+        year = 2017
+
+        if args['year']:
+            year = args['year']
+
+        if args['week']:
+            week = args['week']
+
+        league = League(LEAGUE_ID, year)
+
+        post_to_slack(league.scoreboard(week=week))
+
 api.add_resource(Root, '/')
+api.add_resource(Scoreboard, '/scoreboard/')
