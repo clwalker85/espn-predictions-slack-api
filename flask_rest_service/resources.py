c97a7eedd2bf48ce5dd120eb91e38e14e05ef86b
Chris Walker <clwalker@advent.com>
2017-07-13 16:23:56 -0400

    Attempting to dump prediction responses to logs

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -56,8 +56,7 @@ class Scoreboard(restful.Resource):
 class Prediction(restful.Resource):
     def post(self):
         args = self.parser.parse_args()
-
-        post_text_to_slack(args)
+        print args
 
 class SendPredictionForm(restful.Resource):
     def post(self):
