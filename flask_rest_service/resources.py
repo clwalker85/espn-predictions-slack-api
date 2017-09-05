498a8b6dafa5272f894d570589e7cd42703a7d02
Chris Walker <clwalker@advent.com>
2017-09-05 13:20:51 -0400

    Experimenting with flask responses, part 12

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -61,9 +61,7 @@ class Prediction(restful.Resource):
         #    'text': str(vars(dump)),
         #    'channel': '#test_messages'
         #})
-        return {
-            'replace_original': False
-        }
+        return ''
     def get(self):
         return {
             'replace_original': False
