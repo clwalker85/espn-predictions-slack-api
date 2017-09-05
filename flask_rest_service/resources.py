0ff3348e59e126ff4b6d7f6071c2e9e220b07f78
Chris Walker <clwalker@advent.com>
2017-09-05 14:46:58 -0400

    Added var dump of prediction request form, part 4

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -56,7 +56,7 @@ class Scoreboard(restful.Resource):
 
 class Prediction(restful.Resource):
     def post(self):
-        dump = request.data
+        dump = request.get_data()
         post_to_slack({
             'text': dump,
             'channel': '#test_messages'
