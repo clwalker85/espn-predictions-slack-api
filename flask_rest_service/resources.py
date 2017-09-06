f12d5af4fdd0c8a61b4ca506472543d86b6a4255
Chris Walker <clwalker@advent.com>
2017-09-06 19:21:14 -0400

    Added datetime checks, part 4

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -2,7 +2,7 @@ import json
 import pprint
 import requests
 from espnff import League
-#from datetime import datetime
+from datetime import datetime
 from flask import request, abort, Response
 from flask.ext import restful
 from flask_rest_service import app, api, mongo
@@ -12,8 +12,8 @@ WEBHOOK_URL = 'https://hooks.slack.com/services/T3P5XT2R2/B6Z62CEUU/Zxe31ZDUrpqI
 LEAGUE_MEMBERS = ['Alexis', 'Bryant', 'Cathy', 'Freddy', 'Ian', 'James', 'Joel', 'Justin', 'Kevin', 'Mike', 'Renato', 'Todd', 'Tom', 'Walker']
 LEAGUE_YEAR = '2017'
 LEAGUE_WEEK = '1'
-DEADLINE_STRING = 'Sep 7 2017 8:25PM EST'
-#DEADLINE_TIME = datetime.strptime(DEADLINE_STRING, '%b %d %Y %I:%M%p %Z')
+DEADLINE_STRING = 'Sep 07 2017 8:25PM EST'
+DEADLINE_TIME = datetime.strptime(DEADLINE_STRING, '%b %d %Y  %I:%M%p %Z')
 MATCHUPS = [
     ('Walker versus Renato', 'Walker', 'Renato'),
     ('Bryant versus Mike', 'Bryant', 'Mike'),
@@ -48,8 +48,8 @@ class Scoreboard(restful.Resource):
 
 class Prediction(restful.Resource):
     def post(self):
-        #if datetime.now() > DEADLINE_TIME:
-        #    return Response()
+        if datetime.now() > DEADLINE_TIME:
+            return Response()
 
         payload = json.loads(request.form.get('payload', None))
 
