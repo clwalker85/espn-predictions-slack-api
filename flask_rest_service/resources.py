0aae2d9afe80b19b5c1d152d5e1156b8d1434647
Chris Walker <clwalker@advent.com>
2017-09-08 18:21:34 -0400

    Prediction summary, part 15

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -61,19 +61,18 @@ class PredictionSubmissions(restful.Resource):
         for prediction in mongo.db.predictions.find({ 'year_and_week': year_and_week }):
             username = prediction['username']
             prediction_string = username + ' Winners: '
+            winners_string = ''
+            matchups_string = ''
 
             for attachment in prediction['message']['attachments']:
                 for action in attachment['actions']:
                     if action['type'] == 'button' and action['style'] == 'primary':
-                        prediction_string += action['text'] + ', '
+                        winners_string += action['text'] + ', '
+#                    if action['type'] == 'select' and action['selected_options']:
+#                        for selected in action['selected_options']:
+#                            matchups_string += attachment['text'] + ': ' + selected['text'] + '\n'
 
-            prediction_string = prediction_string.rstrip(', ') + '\n'
-
-            for attachment in prediction['message']['attachments']:
-                for action in attachment['actions']:
-                    if action['type'] == 'select' and action['selected_options']:
-                        for selected in action['selected_options']:
-                            prediction_string += attachment['text'] + ': ' + selected['text'] + '\n'
+            prediction_string = winners_string.rstrip(', ') + '\n' + matchups_string
 
 #            score_prediction = mongo.db.score_predictions.find_one({ 'username': username, 'year_and_week': year_and_week })
 #            prediction_string += 'Highest Score: ' + score_prediction['high_score'] + '\n'
