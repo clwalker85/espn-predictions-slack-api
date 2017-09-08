c49d41104b69dab86009149a30beea75b328cce3
Chris Walker <clwalker@advent.com>
2017-09-08 18:24:53 -0400

    Prediction summary, part 16

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -72,7 +72,7 @@ class PredictionSubmissions(restful.Resource):
 #                        for selected in action['selected_options']:
 #                            matchups_string += attachment['text'] + ': ' + selected['text'] + '\n'
 
-            prediction_string = winners_string.rstrip(', ') + '\n' + matchups_string
+            prediction_string += winners_string.rstrip(', ') + '\n' + matchups_string
 
 #            score_prediction = mongo.db.score_predictions.find_one({ 'username': username, 'year_and_week': year_and_week })
 #            prediction_string += 'Highest Score: ' + score_prediction['high_score'] + '\n'
