1ec4bf91ad1c1191672660dce1fd9299f1cb5cf0
Chris Walker <clwalker@advent.com>
2017-09-08 18:04:45 -0400

    Prediction summary, part 9

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -63,20 +63,20 @@ class PredictionSubmissions(restful.Resource):
             prediction_string = username + ' Winners: '
 
             for attachment in prediction['message']['attachments']:
-                for action in actions:
+                for action in attachment['actions']:
                     if action['type'] == 'button' and action['style'] == 'primary':
                         prediction_string += action['text'] + ', '
 
-#                prediction_string = prediction_string.rstrip(',') + '\n'
-#
-#                for action in actions:
-#                    if action['type'] == 'select' and action['selected_options']:
-#                        for selected in action['selected_options']:
-#                            prediction_string += attachment['text'] + ': ' + selected['text'] + '\n'
-#
-#            score_prediction = mongo.db.score_predictions.find_one({ 'username': username, 'year_and_week': year_and_week })
-#            prediction_string += 'Highest Score: ' + score_prediction['high_score'] + '\n'
-#            prediction_string += 'Lowest Score: ' + score_prediction['low_score']
+                prediction_string = prediction_string.rstrip(',') + '\n'
+
+                for action in attachment['actions']:
+                    if action['type'] == 'select' and action['selected_options']:
+                        for selected in action['selected_options']:
+                            prediction_string += attachment['text'] + ': ' + selected['text'] + '\n'
+
+            score_prediction = mongo.db.score_predictions.find_one({ 'username': username, 'year_and_week': year_and_week })
+            prediction_string += 'Highest Score: ' + score_prediction['high_score'] + '\n'
+            prediction_string += 'Lowest Score: ' + score_prediction['low_score']
 
             message['attachments'].append({ 'text': prediction_string })
 
