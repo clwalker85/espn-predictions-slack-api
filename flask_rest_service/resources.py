cc2aa95abaaa3c103ad40685b279466eb442f02e
Chris Walker <clwalker@advent.com>
2017-09-10 23:33:45 -0400

    Prediction calculations, part 36

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -239,8 +239,8 @@ class PredictionCalculations(restful.Resource):
                 mongo.db.prediction_standings.insert_one({
                     'username': user_formula['username'],
                     'year_and_week': year_and_week,
-                    'total': NumberInt(0),
-                    'low': NumberInt(formula_total)
+                    'total': int(0),
+                    'low': int(formula_total)
                 })
             user_formula_string = user_formula['username'] + ': ' + str(formula_total) + ' = ' + str(user_formula['matchup_total']) + ' + ' + str(user_formula['blowout_bonus']) + ' + ' + str(user_formula['closest_bonus']) + ' + ' + str(user_formula['highest_bonus']) + ' + ' + str(user_formula['lowest_bonus'])
             formula_string += user_formula_string + '\n'
