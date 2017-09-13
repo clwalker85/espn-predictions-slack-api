8ffedaa01b97df4cf66e6dcb8da2b7fd188e1d9d
Chris Walker <clwalker@advent.com>
2017-09-12 23:43:08 -0400

    Adding slackclient API calls to post bot messages, part 16

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -36,16 +36,18 @@ def post_to_slack(payload):
 
     dm_channel_list = sc.api_call("im.list")
 
-    for channel in dm_channel_list:
-        #if channel['user'] in LEAGUE_USERNAMES:
-        user = sc.api_call('users.info', user=channel['user'])
-        if user['name'] == 'clwalker':
-            sc.api_call("chat.postMessage",
-                channel=channel['id'],
-                text=payload['text'],
-                attachments=payload['attachments'],
-                as_user=False
-            )
+    pprint.pformat(dm_channel_list)
+
+#    for channel in dm_channel_list:
+#        #if channel['user'] in LEAGUE_USERNAMES:
+#        user = sc.api_call('users.info', user=channel['user'])
+#        if user['name'] == 'clwalker':
+#            sc.api_call("chat.postMessage",
+#                channel=channel['id'],
+#                text=payload['text'],
+#                attachments=payload['attachments'],
+#                as_user=False
+#            )
     return
 
 class Root(restful.Resource):
