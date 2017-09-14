c7298b6cf3d41761adae81b2635c8b18a32cd5a6
Chris Walker <clwalker@advent.com>
2017-09-14 10:32:29 -0400

    Debugging prediction submission, part 4

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -210,11 +210,13 @@ class Prediction(restful.Resource):
                                 if option['value'] == selected['value']:
                                     element['selected_options'].append(option)
 
-#        mongo.db.predictions.update(database_key, {
-#            '$set': {
-#                'message': message
-#            },
-#        }, upsert=True, multi=False)
+        result = mongo.db.predictions.update(database_key, {
+            '$set': {
+                'message': message
+            },
+        }, upsert=True, multi=False)
+
+        print(pprint.pformat(result))
 
         ## Slack replaces old prediction form with any immediate response,
         ## so return the form again with any selected buttons styled
