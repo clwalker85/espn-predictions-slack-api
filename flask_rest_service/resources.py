e855e560f0dac982f687f0a25fd7b9cecdc8004d
Chris Walker <clwalker@advent.com>
2017-10-11 09:52:07 -0400

    Hard-coding week 6 matchups

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -17,20 +17,20 @@ LEAGUE_USERNAMES = ['alexis', 'bernie', 'wildcougar', 'freddy', 'imcguigan', 'jt
 # same order as arrays above
 LEAGUE_USER_IDS = ['U3P2770FK', 'U3P6D7PT7', 'U3P6LPVLM', 'U3P3NU4E6', 'U6T9Y84LS', 'U5C50S29H', 'U5RV1SGSE', 'U5TQ9NKEX', 'U3P4HLXD0', 'U5SF98KMX', 'U4L7RTJ30', 'U3PHKK00L', 'U3NEWLZFS', 'U3NE3S6CQ' ]
 LEAGUE_YEAR = '2017'
-LEAGUE_WEEK = '5'
-DEADLINE_STRING = 'October 5th, 2017, at 08:25PM'
+LEAGUE_WEEK = '6'
+DEADLINE_STRING = 'October 12th, 2017, at 08:25PM'
 # UTC version of time above - https://www.worldtimebuddy.com/
-DEADLINE_TIME = datetime.strptime('October 06 2017 12:25AM', '%B %d %Y %I:%M%p')
+DEADLINE_TIME = datetime.strptime('October 13 2017 12:25AM', '%B %d %Y %I:%M%p')
 # UTC version of Tuesday @ 8AM of that week; remember leading zeroes in days!
-WEEK_END_TIME = datetime.strptime('October 10 2017 12:00PM', '%B %d %Y %I:%M%p')
+WEEK_END_TIME = datetime.strptime('October 17 2017 12:00PM', '%B %d %Y %I:%M%p')
 MATCHUPS = [
-    ('Walker versus Tom', 'Walker', 'Tom'),
-    ('Bryant versus James', 'Bryant', 'James'),
-    ('Cathy versus Todd', 'Cathy', 'Todd'),
-    ('Joel versus Kevin', 'Joel', 'Kevin'),
-    ('Renato versus Mike', 'Renato', 'Mike'),
-    ('Ian versus Justin', 'Ian', 'Justin'),
-    ('Alexis versus Freddy', 'Alexis', 'Freddy'),
+    ('Walker versus Bryant', 'Walker', 'Bryant'),
+    ('James versus Cathy', 'James', 'Cathy'),
+    ('Tom versus Joel', 'Tom', 'Joel'),
+    ('Todd versus Renato', 'Todd', 'Renato'),
+    ('Kevin versus Ian', 'Kevin', 'Ian'),
+    ('Mike versus Alexis', 'Mike', 'Alexis'),
+    ('Justin versus Freddy', 'Justin', 'Freddy'),
 ]
 
 def post_to_slack(payload):
@@ -538,10 +538,7 @@ class PredictionCalculations(restful.Resource):
                         },
                     }, upsert=True, multi=False)
 
-        print('before for loop')
         for prediction_record in mongo.db.prediction_standings.find({ 'year_and_week': year_and_week }).sort([('total', -1), ('low', -1)]):
-            print('in for loop')
-            print(pprint.pformat(prediction_record))
             standings_string += prediction_record['username'] + ' - ' + str(prediction_record['total']) + '; LOW: ' + str(prediction_record['low']) + '\n'
         message['attachments'].append({ 'text': standings_string })
 
