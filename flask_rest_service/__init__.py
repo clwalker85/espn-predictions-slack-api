4ee37ab167c2d6f8e446d250233d20c86673b7a7
Chris Walker <clwalker@advent.com>
2020-11-04 02:20:15 -0500

    Fixed slackclient import after pymongo version bump, take 5

--- flask_rest_service/__init__.py
+++ flask_rest_service/__init__.py
@@ -100,7 +100,7 @@ def post_to_slack(payload):
     for user_id in LEAGUE_USER_IDS:
 	# uncomment this line to send shit only to Walker
         if user_id in [ 'U3NE3S6CQ' ]:
-            channel = sc.api_call('im.open', user=user_id)
+            channel = sc.im_open(user=user_id)
 
             if 'channel' in channel:
                 channel = channel['channel']
