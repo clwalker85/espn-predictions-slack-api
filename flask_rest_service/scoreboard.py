1f2ba35806c1fb981869f63c1444e3d610cd8182
Chris Walker <clwalker@advent.com>
2021-09-08 18:36:44 -0400

    Added endpoint for head to head history, part 12

--- flask_rest_service/scoreboard.py
+++ flask_rest_service/scoreboard.py
@@ -104,14 +104,14 @@ class GetHeadToHeadHistory(restful.Resource):
             else:
                 matchup_string += manager_two + ' ' + str(manager_two_reg_season_wins) + '-' + str(manager_one_reg_season_wins) + ' ' + manager_one
 
-            if (manager_one_playoff_wins + manager_two_playoff_wins) + 0:
+            if (manager_one_playoff_wins + manager_two_playoff_wins) > 0:
                 # keep same order as regular season record
                 if manager_one_reg_season_wins > manager_two_reg_season_wins:
                     matchup_string += ', ' + manager_one_playoff_wins + '-' + manager_two_playoff_wins + 'in playoffs'
                 else:
                     matchup_string += ', ' + manager_two_playoff_wins + '-' + manager_one_playoff_wins + 'in playoffs'
 
-            if (manager_one_consolation_wins + manager_two_consolation_wins) + 0:
+            if (manager_one_consolation_wins + manager_two_consolation_wins) > 0:
                 # keep same order as regular season record
                 if manager_one_reg_season_wins > manager_two_reg_season_wins:
                     matchup_string += ', ' + manager_one_consolation_wins + '-' + manager_two_consolation_wins + 'in consolation'
