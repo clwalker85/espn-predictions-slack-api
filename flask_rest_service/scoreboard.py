773a5ac07486efde90811fa15653cf9d8e9d17b3
Chris Walker <clwalker@advent.com>
2021-09-08 18:47:33 -0400

    Added endpoint for head to head history, part 17

--- flask_rest_service/scoreboard.py
+++ flask_rest_service/scoreboard.py
@@ -114,9 +114,9 @@ class GetHeadToHeadHistory(restful.Resource):
             if (manager_one_consolation_wins + manager_two_consolation_wins) > 0:
                 # keep same order as regular season record
                 if manager_one_reg_season_wins > manager_two_reg_season_wins:
-                    matchup_string += ', ' + str(manager_one_consolation_wins) + '-' + manager_two_consolation_wins + 'in consolation'
+                    matchup_string += ', ' + str(manager_one_consolation_wins) + '-' + str(manager_two_consolation_wins) + 'in consolation'
                 else:
-                    matchup_string += ', ' + str(manager_two_consolation_wins) + '-' + manager_one_consolation_wins + 'in consolation'
+                    matchup_string += ', ' + str(manager_two_consolation_wins) + '-' + str(manager_one_consolation_wins) + 'in consolation'
 
             # one message attachment per matchup
             message['attachments'].append({ 'text': matchup_string })
